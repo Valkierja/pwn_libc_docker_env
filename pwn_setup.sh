1a625b6142312136c4bf711cfcbc7565174a7bc6
冷罗KS <68413810+KSroido@users.noreply.github.com>
2021-11-18 08:46:49 +0800

    Update pwn_setup.sh

--- pwn_setup.sh
+++ pwn_setup.sh
@@ -6,7 +6,9 @@ passwd
 sed -i 's/# deb-src/deb-src/' "/etc/apt/sources.list"
 dpkg --add-architecture i386
 apt-get update
-apt-get -y install libc6-i386 git gdb which source libc6-dev vim
+apt-get -y install libc6-i386 git gdb which source libc6-dev vim libglib2.0 
+apt-get -y install libglib2.0-dev
+apt-get -y install libglib2.0-devel
 apt-get -y install python2 python2-pip
 git clone https://github.com/pwndbg/pwndbg
 cd pwndbg
